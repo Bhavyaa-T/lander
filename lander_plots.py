3e0641d548599668ea304e6e5be57561e3a897cf
Bhavyaa-T <bt437@cam.ac.uk>
2025-10-02 23:50:18 +0100

    plots + trajectories

---
+++ lander_plots.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+results = np.loadtxt('trajectories.txt')
+plt.figure(1)
+plt.clf()
+plt.xlabel('time (s)')
+plt.grid()
+plt.plot(results[:, 0], results[:, 1], label='x (m)')
+plt.legend()
+plt.show()
+
+plt.figure(2)
+plt.clf()
+plt.xlabel('time (s)')
+plt.grid()
+plt.plot(results[:, 0], results[:, 2], label='v (m/s)')
+plt.legend()
+plt.show()
